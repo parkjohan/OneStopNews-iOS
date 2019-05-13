deb5f1f3d464e59571dc28603ead18176e6966a2
Johan Park <parkjohan@gmail.com>
2019-05-13 15:17:33 -0700

    Initial Commit

---
+++ OneStopNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneStopNews
+//
+//  Created by Johan Park on 5/13/19.
+//  Copyright © 2019 Johan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
